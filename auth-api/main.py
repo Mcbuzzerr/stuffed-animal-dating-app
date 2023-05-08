9f86208eb2c556dd99a10639df2764258fbdb6f4
Alex <mcbuzzer@gmail.com>
2023-05-08 17:26:39 -0600

    SQLAlchemy connection works

--- auth-api/main.py
+++ auth-api/main.py
@@ -9,14 +9,24 @@ from fastapi_jwt_auth.exceptions import AuthJWTException
 import py_eureka_client.eureka_client as eureka_client
 from contextlib import asynccontextmanager
 
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+
+# TUTORIAL: https://docs.sqlalchemy.org/en/20/tutorial/metadata.html
+
 
 @asynccontextmanager
 async def startup(app: FastAPI):
     # connect to SQL database
+    app.engine = create_engine(
+        "mssql+pyodbc://sa:Password123@auth-db/Users?driver=ODBC+Driver+17+for+SQL+Server",
+        echo=True,
+    )
 
+    # un-comment this when we have a working eureka server
     await eureka_client.init_async(
-        eureka_server="http://eureka:8761/eureka",
-        app_name="user-api",
+        eureka_server="http://sada-eureka:8761/eureka",
+        app_name="auth-api",
         instance_port=8000,
     )
 
@@ -48,6 +58,13 @@ async def authjwt_exception_handler(request: Request, exc: AuthJWTException):
 async def root(Authorize: AuthJWT = Depends()):
     Authorize.jwt_optional()
     current_user = Authorize.get_jwt_subject()
+    with Session(app.engine) as session:
+        results = session.execute(text("SELECT * FROM UserAuth"))
+        for result in results:
+            print(result)
+    #   session.execute("INSERT INTO Users (params) VALUES (params)", [{params},{params}])
+    #   session.commit()
+
     if current_user is None:
         return {"message": "Hello World"}
     return {"message": f"Hello {current_user}"}
